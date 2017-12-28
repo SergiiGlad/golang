1bed96042bbb6a6367f9f3db65b7af1f8100d38e
Sergii Gladchenko <gladseo@gmail.com>
2017-12-28 13:00:12 +0000

    add file arithmetical function

---
+++ arith_func.go
@@ -0,0 +1,54 @@
+	package main
+
+	import 	"fmt"
+
+
+
+	func main() {
+
+		array := []int{1,2,3,4,5}
+
+		fmt.Println("Sum ", array, " = ", sum(array))
+
+		fmt.Println("Multiply ", array, " = ", multiply(array))
+
+
+
+
+
+
+	}
+
+
+
+
+
+	// sums all the number in a slice of number and return result
+
+	func sum(array1 []int) (sum int) {
+
+		i := len( array1 );
+
+		for	 i > 0 {
+
+			sum += array1[i-1]
+			i--
+		}
+
+		return
+	}
+
+	func multiply(array2 []int) int {
+
+		multi := 1
+
+		for i := 0; i < len(array2); i++ {
+
+			multi *= array2[i]
+
+		}
+
+		return multi
+
+	}
+
