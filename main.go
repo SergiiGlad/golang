dd05f68f6735cf210e0d4c1e1e072542aa409356
Sergii Gladchenko <gladseo@gmail.com>
2018-01-15 15:12:22 +0000

    add main func

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
-package main()
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi , %s, \n Nice to meet you !\n", r.URL.Path[1:])
+}
 
 func main() {
-    
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
