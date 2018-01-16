6aecb0b28ee5de56da9ef06b230bbf8b25374029
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2018-01-16 21:07:30 +0200

    fixed vue

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+  "html/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi , %s, \n Nice to meet you !\n", r.URL.Path[1:])
+    tmpl, err := template.ParseFiles("client/index.html")
+    if err != nil {
+      fmt.Fprintf(w, "%s", "Error")
+    } else {
+      tmpl.Execute(w, r)
+    }
+    //fmt.Fprintf(w, "Hello Home! %s", r.URL.Path[1:]) )
+
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+  http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("client/dist"))))
 	http.ListenAndServe(":8080", nil)
 }
