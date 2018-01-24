ab01433ac91a42b98f30178c8c3083c350ad78c2
Denis Kononenko <denis1kononenko@gmail.com>
2018-01-24 18:37:27 +0200

    check

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-  "html/template"
   "go-team-room/conf"
+  "github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("client/index.html")
-    if err != nil {
-      fmt.Fprintf(w, "%s", "Error")
-    } else {
-      tmpl.Execute(w, r)
-    }
-    //fmt.Fprintf(w, "Hello Home! %s", r.URL.Path[1:]) )
-
-}
-
 func main() {
-  http.HandleFunc("/", handler)
-  http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("client/dist"))))
-  http.Handle("/api-docs/", http.StripPrefix("/api-docs/", http.FileServer(http.Dir("swagger"))))
-  http.ListenAndServe(conf.Ip + ":" + conf.Port, nil)
-
+  r := mux.NewRouter()
+  http.ListenAndServe(conf.Port, r)
 }
