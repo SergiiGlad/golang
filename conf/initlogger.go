d7d365c1415ac51f3749ff7ae6f44ecadb641dc7
Sergii Gladchenko <gladseo@gmail.com>
2018-02-01 15:00:58 +0000

    add settings for init logger

---
+++ conf/initlogger.go
@@ -0,0 +1,114 @@
+// Package logs provide a general log settings
+// It defines a type logrus Logger, with methods for formatting output.
+// Let see ./conf/sample_log.go for example ho uses logrus Logger
+
+package conf
+
+import (
+	"fmt"
+	"github.com/heirko/go-contrib/logrusHelper"
+	"github.com/heralight/logrus_mate"
+	rotatelogs "github.com/lestrrat/go-file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"strconv"
+	"time"
+)
+
+// The variable log is a name for our logger
+var log *logrus.Logger
+
+// A function to fetch current logger
+func GetLog() *logrus.Logger {
+	return log
+}
+
+// Init logger
+func init() {
+
+	// Init Viper for read configuration file ./conf.json
+	var viper = viper.New()
+
+	// name of config file (without extension), here we use some logrus_mate sample
+	viper.SetConfigName("conf")
+
+	// optionally look for config in the working directory
+	viper.AddConfigPath("./conf")
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	// Handle errors reading the config file
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	// End Init Viper
+
+	// new hook, you just need a registration
+	// beacause logrus_mate doesn't have a lsfhook in package
+	logrus_mate.RegisterHook("lfshook", NewHook)
+
+	// Read and unmarshal configuration from viper
+
+	var mate_conf logrus_mate.LoggerConfig
+	mate_conf = logrusHelper.UnmarshalConfiguration(viper)
+
+	// create logger's instance
+	log = logrus.New()
+
+	// apply the configuration to logger
+	if logrusHelper.SetConfig(log, mate_conf); err != nil {
+		// Handle errors reading the mate config
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	// end logger setup configuration
+
+	log.Info("Logger configuration has finished")
+
+}
+
+// If you want to use your own hook, you just need todo as follow
+func NewHook(options logrus_mate.Options) (hook logrus.Hook, err error) {
+
+	// name of field viper file conf.json
+	logfile := "logfile"
+	rotatehours := "rotatehours"
+	maxdays := "maxdays"
+
+	filename, err := options.String(logfile)
+
+	//Interval between file rotation.
+	//By default logs are rotated every 86400 seconds.
+	//Note: Remember to use time.Duration values.
+	rotationTime, err := options.String(rotatehours)
+	maxAge, err := options.String(maxdays)
+
+	rotation, err := strconv.ParseInt(rotationTime, 10, 64)
+	age, err := strconv.ParseInt(maxAge, 10, 64)
+
+	writer, err := rotatelogs.New(
+		filename+".%Y%m%d%H%M",
+		rotatelogs.WithLinkName(filename),
+		rotatelogs.WithMaxAge(time.Duration(86400*age)*time.Second),
+		rotatelogs.WithRotationTime(time.Duration(3600*rotation)*time.Second),
+		rotatelogs.WithClock(rotatelogs.UTC),
+	)
+
+	pathMap := lfshook.WriterMap{
+		logrus.InfoLevel:  writer,
+		logrus.ErrorLevel: writer,
+	}
+
+	hook = lfshook.NewHook(
+		pathMap,
+		&logrus.JSONFormatter{},
+	)
+
+	if err = options.ToObject(&hook); err != nil {
+		return
+	}
+
+	return
+}
--- controllers/userservice.go
+++ controllers/userservice.go
@@ -1,247 +1,250 @@
 package controllers
 
 import (
-  "go-team-room/models/dao"
-  "regexp"
-  "errors"
-  "gopkg.in/hlandau/passlib.v1/hash/bcrypt"
-  "log"
-  "database/sql"
-  "strings"
-  "go-team-room/models/dto"
-  "go-team-room/models/dao/interfaces"
+	"database/sql"
+	"errors"
+	"go-team-room/conf"
+	"go-team-room/models/dao"
+	"go-team-room/models/dao/interfaces"
+	"go-team-room/models/dto"
+	"gopkg.in/hlandau/passlib.v1/hash/bcrypt"
+	"regexp"
+	"strings"
 )
 
+// Get instance of logger (Formatter, Hook，Level, Output ).
+// If you want to use only your log message  It will need use own call logs example
+var log = conf.GetLog()
+
 type UserService struct {
-  Dao interfaces.UserDao
+	Dao interfaces.UserDao
 }
 
 var _ UserServiceInterface = &UserService{}
 
 func (us *UserService) CreateUser(userDto *dto.RequestUserDto) (dto.ResponseUserDto, error) {
 
-  userEntity := dto.RequestUserDtoToEntity(userDto)
-  var respUserDto dto.ResponseUserDto
+	userEntity := dto.RequestUserDtoToEntity(userDto)
+	var respUserDto dto.ResponseUserDto
 
-  err := checkUniqueEmail(userEntity.Email, us.Dao)
+	err := checkUniqueEmail(userEntity.Email, us.Dao)
 
-  if err != nil && err != sql.ErrNoRows {
-    return respUserDto, err
-  }
+	if err != nil && err != sql.ErrNoRows {
+		return respUserDto, err
+	}
 
-  err = checkUniquePhone(userEntity.Phone, us.Dao)
+	err = checkUniquePhone(userEntity.Phone, us.Dao)
 
-  if err != nil && err != sql.ErrNoRows {
-    return respUserDto, err
-  }
+	if err != nil && err != sql.ErrNoRows {
+		return respUserDto, err
+	}
 
-  if validPasswordLength(userEntity.CurrentPass) == false {
-    return respUserDto, errors.New("Password too short.")
-  }
+	if validPasswordLength(userEntity.CurrentPass) == false {
+		return respUserDto, errors.New("Password too short.")
+	}
 
-  hashPass, err := bcrypt.Crypter.Hash(userEntity.CurrentPass)
+	hashPass, err := bcrypt.Crypter.Hash(userEntity.CurrentPass)
 
-  if err != nil {
-    return respUserDto, err
-  }
+	if err != nil {
+		return respUserDto, err
+	}
 
-  userEntity.CurrentPass = hashPass
-  nameLetterToUppep(&userEntity)
+	userEntity.CurrentPass = hashPass
+	nameLetterToUppep(&userEntity)
 
-  _, err = us.Dao.AddUser(&userEntity)
+	_, err = us.Dao.AddUser(&userEntity)
 
-  if err != nil {
-    log.Println(err)
-    return respUserDto, err
-  }
+	if err != nil {
+		log.Println(err)
+		return respUserDto, err
+	}
 
-  respUserDto = dto.UserEntityToResponseDto(&userEntity)
+	respUserDto = dto.UserEntityToResponseDto(&userEntity)
 
-  return respUserDto, nil
+	return respUserDto, nil
 }
 
 func (us *UserService) UpdateUser(id int64, userDto *dto.RequestUserDto) (dto.ResponseUserDto, error) {
 
-  newUserData := dto.RequestUserDtoToEntity(userDto)
-  oldUserData, err := us.Dao.FindUserById(id)
-  var responseUserDto dto.ResponseUserDto
+	newUserData := dto.RequestUserDtoToEntity(userDto)
+	oldUserData, err := us.Dao.FindUserById(id)
+	var responseUserDto dto.ResponseUserDto
 
-  if err != nil {
-    return responseUserDto, err
-  }
+	if err != nil {
+		return responseUserDto, err
+	}
 
-  if len(newUserData.FirstName) == 0 {
-    newUserData.FirstName = oldUserData.FirstName
-  }
+	if len(newUserData.FirstName) == 0 {
+		newUserData.FirstName = oldUserData.FirstName
+	}
 
-  if len(newUserData.SecondName) == 0 {
-    newUserData.SecondName = oldUserData.SecondName
-  }
+	if len(newUserData.SecondName) == 0 {
+		newUserData.SecondName = oldUserData.SecondName
+	}
 
-  if len(newUserData.Email) != 0 {
-    err = checkUniqueEmail(newUserData.Email, us.Dao)
+	if len(newUserData.Email) != 0 {
+		err = checkUniqueEmail(newUserData.Email, us.Dao)
 
-    if err != nil && err != sql.ErrNoRows {
-      return responseUserDto, err
-    }
-  } else {
-    newUserData.Email = oldUserData.Email
-  }
+		if err != nil && err != sql.ErrNoRows {
+			return responseUserDto, err
+		}
+	} else {
+		newUserData.Email = oldUserData.Email
+	}
 
-  if len(newUserData.Phone) != 0 {
-    err = checkUniquePhone(newUserData.Phone, us.Dao)
+	if len(newUserData.Phone) != 0 {
+		err = checkUniquePhone(newUserData.Phone, us.Dao)
 
-    if err != nil && err != sql.ErrNoRows {
-      return responseUserDto, err
-    }
-  } else {
-    newUserData.Phone = oldUserData.Phone
-  }
+		if err != nil && err != sql.ErrNoRows {
+			return responseUserDto, err
+		}
+	} else {
+		newUserData.Phone = oldUserData.Phone
+	}
 
-  if len(newUserData.CurrentPass) != 0 {
-    if validPasswordLength(newUserData.CurrentPass) == false {
-      return responseUserDto, errors.New("Password too short.")
-    }
+	if len(newUserData.CurrentPass) != 0 {
+		if validPasswordLength(newUserData.CurrentPass) == false {
+			return responseUserDto, errors.New("Password too short.")
+		}
 
-    hashPass, err := bcrypt.Crypter.Hash(newUserData.CurrentPass)
+		hashPass, err := bcrypt.Crypter.Hash(newUserData.CurrentPass)
 
-    if err != nil {
-      log.Println(err)
-      return responseUserDto, err
-    }
+		if err != nil {
+			log.Println(err)
+			return responseUserDto, err
+		}
 
-    newUserData.CurrentPass = hashPass
-  } else {
-    newUserData.CurrentPass = oldUserData.CurrentPass
-  }
+		newUserData.CurrentPass = hashPass
+	} else {
+		newUserData.CurrentPass = oldUserData.CurrentPass
+	}
 
-  nameLetterToUppep(&newUserData)
+	nameLetterToUppep(&newUserData)
 
-  err = us.Dao.UpdateUser(id, &newUserData)
-  if err != nil {
-    log.Println(err)
-    return responseUserDto, err
-  }
+	err = us.Dao.UpdateUser(id, &newUserData)
+	if err != nil {
+		log.Println(err)
+		return responseUserDto, err
+	}
 
-  newUserData.ID = id
-  responseUserDto = dto.UserEntityToResponseDto(&newUserData)
-  responseUserDto.Friends, _ = us.GetUserFriends(id)
+	newUserData.ID = id
+	responseUserDto = dto.UserEntityToResponseDto(&newUserData)
+	responseUserDto.Friends, _ = us.GetUserFriends(id)
 
-  return responseUserDto, nil
+	return responseUserDto, nil
 }
 
 func (us *UserService) DeleteUser(id int64) (dto.ResponseUserDto, error) {
 
-  var responseUserDto dto.ResponseUserDto
+	var responseUserDto dto.ResponseUserDto
 
-  userEntity, err := us.Dao.FindUserById(id)
+	userEntity, err := us.Dao.FindUserById(id)
 
-  if err != nil {
-    return responseUserDto, err
-  }
+	if err != nil {
+		return responseUserDto, err
+	}
 
-  responseUserDto = dto.UserEntityToResponseDto(userEntity)
-  responseUserDto.Friends, _ = us.GetUserFriends(id)
+	responseUserDto = dto.UserEntityToResponseDto(userEntity)
+	responseUserDto.Friends, _ = us.GetUserFriends(id)
 
-  return responseUserDto, us.Dao.DeleteUser(id)
+	return responseUserDto, us.Dao.DeleteUser(id)
 }
 
 func (us *UserService) GetUserFriends(id int64) ([]int64, error) {
-  _, err := us.Dao.FindUserById(id)
+	_, err := us.Dao.FindUserById(id)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  return us.Dao.FriendsByUserID(id)
+	return us.Dao.FriendsByUserID(id)
 }
 
 func checkUniqueEmail(email string, dao interfaces.UserDao) error {
 
-  if validEmail(email) == false {
-    return errors.New("Invalid email format.")
-  } else {
+	if validEmail(email) == false {
+		return errors.New("Invalid email format.")
+	} else {
 
-    _, err := dao.FindUserByEmail(email)
+		_, err := dao.FindUserByEmail(email)
 
-    switch err {
-    case sql.ErrNoRows:
-      return err
+		switch err {
+		case sql.ErrNoRows:
+			return err
 
-    case nil:
-      return errors.New("There is user with such email.")
+		case nil:
+			return errors.New("There is user with such email.")
 
-    default:
-      log.Println(err)
-      return err
-    }
-  }
+		default:
+			log.Println(err)
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func checkUniquePhone(phone string, dao interfaces.UserDao) error {
-  if len(phone) > 0 {
+	if len(phone) > 0 {
 
-    if validPhone(phone) == false {
-      return errors.New("Invalid phone number format.")
-    } else {
+		if validPhone(phone) == false {
+			return errors.New("Invalid phone number format.")
+		} else {
 
-      _, err := dao.FindUserByPhone(phone)
+			_, err := dao.FindUserByPhone(phone)
 
-      switch err {
-      case sql.ErrNoRows:
-        return err
+			switch err {
+			case sql.ErrNoRows:
+				return err
 
-      case nil:
-        return errors.New("There is user with such phone.")
+			case nil:
+				return errors.New("There is user with such phone.")
 
-      default:
-        log.Println(err)
-        return err
-      }
-    }
-  }
+			default:
+				log.Println(err)
+				return err
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func validRegexItem(item string, pattern string) bool {
-  itemRegex := regexp.MustCompile(pattern)
+	itemRegex := regexp.MustCompile(pattern)
 
-  if isItemOk := itemRegex.MatchString(item); isItemOk == false {
-    return false
-  }
+	if isItemOk := itemRegex.MatchString(item); isItemOk == false {
+		return false
+	}
 
-  return true
+	return true
 }
 
 func validEmail(email string) bool {
-  return validRegexItem(email, "^[a-z0-9]+@[a-z]+[.][a-z]+$")
+	return validRegexItem(email, "^[a-z0-9]+@[a-z]+[.][a-z]+$")
 }
 
 func validPhone(phone string) bool {
-  return validRegexItem(phone, "^[+][0-9]{12}")
+	return validRegexItem(phone, "^[+][0-9]{12}")
 }
 
 func validCyrillicName(name string) bool {
-  return validRegexItem(name, "^[А-Я][а-я]{1,49}")
+	return validRegexItem(name, "^[А-Я][а-я]{1,49}")
 }
 
 func validLatinName(name string) bool {
-  return validRegexItem(name, "^[A-Z][a-z]{1,49}")
+	return validRegexItem(name, "^[A-Z][a-z]{1,49}")
 }
 
 func validPasswordLength(password string) bool {
-  if len(password) < 6 {
-    return false
-  }
+	if len(password) < 6 {
+		return false
+	}
 
-  return true
+	return true
 }
 
 func nameLetterToUppep(user *dao.User) {
-  user.FirstName = strings.ToUpper(string([]rune(user.FirstName)[0])) + string([]rune(user.FirstName)[1:])
-  user.SecondName = strings.ToUpper(string([]rune(user.SecondName)[0])) + string([]rune(user.SecondName)[1:])
+	user.FirstName = strings.ToUpper(string([]rune(user.FirstName)[0])) + string([]rune(user.FirstName)[1:])
+	user.SecondName = strings.ToUpper(string([]rune(user.SecondName)[0])) + string([]rune(user.SecondName)[1:])
 }
-
