942d3ca1111a4f5d2d911c4f2b86fc192a166996
Sergii Gladchenko <gladseo@gmail.com>
2018-02-02 12:09:47 +0000

    logger config

--- conf/initlogger.go
+++ conf/initlogger.go
@@ -42,18 +42,24 @@ func init() {
 	log = logrus.New()
 
 	// read configuration from conf/conf.json and setup logger
-	readSetupLogger(log)
+	readSetupLogger()
 
 	// end logger setup configuration
 	log.Info("Logger configuration has finished")
 
+	// test messages
+	log.Debug("Debug level message from gohum ")        //"Useful debugging information."
+	log.Info("Info level message from gohum")           //"Something noteworthy happened!"
+	log.Warn("Warn level message from gohum")           //You should probably take a look at this."
+	log.Error("Something failed but I'm not quitting.") //Something failed but I'm not quitting.
+
 }
 
-func readSetupLogger(log *logrus.Logger) {
+func readSetupLogger() {
 
 	// new hook, you just need a registration
 	// beacause logrus_mate doesn't have a lsfhook in package
-	// func newHook()
+	// func newHook func(logrus_mate.Options) (hook logrus.Hook, err error)
 	logrus_mate.RegisterHook("lfshook", newHook)
 
 	// Read and unmarshal configuration from viper
@@ -70,45 +76,86 @@ func readSetupLogger(log *logrus.Logger) {
 // If you want to use your own hook, you just need todo as follow
 func newHook(options logrus_mate.Options) (hook logrus.Hook, err error) {
 
-	// name of field viper file conf.json
+	logHook := log.WithFields(logrus.Fields{
+		"Setup Hook": "lfshoook",
+		"Useing":     "default values"})
+
+	// names of fields which will be parsed from viper file conf.json
+	// section "hooks"
+
+	formatter := "formatter"
 	logfile := "logfile"
 	rotatehours := "rotatehours"
 	maxdays := "maxdays"
 
+	//default values for settings
+	filenameDeafult := "/logs/current.log"
+	rotationTimeDefault := "24" // 24 hours
+	maxDaysDefault := "7"
+	formatterDefault := "text"
+
 	filename, err := options.String(logfile)
 	if err != nil {
-		filename = viper.GetString("work_dir") + "/logs/current.log"
-		logrus.Info("Useing default name for logs file current.log")
-	} else {
-		filename = viper.GetString("work_dir") + filename
+		logHook.Error(err)
+		filename = filenameDeafult
 	}
 
 	//Interval between file rotation.
 	//By default logs are rotated every 86400 seconds.
 	//Note: Remember to use time.Duration values.
 	rotationTime, err := options.String(rotatehours)
-	maxAge, err := options.String(maxdays)
-
+	if err != nil {
+		logHook.Error(err)
+		rotationTime = rotationTimeDefault
+	}
 	rotation, err := strconv.ParseInt(rotationTime, 10, 64)
+
+	maxAge, err := options.String(maxdays)
+	if err != nil {
+		logHook.Error(err)
+		maxAge = maxDaysDefault
+	}
 	age, err := strconv.ParseInt(maxAge, 10, 64)
 
 	writer, err := rotatelogs.New(
-		filename+".%Y%m%d%H%M",
-		rotatelogs.WithLinkName(filename),
+		viper.GetString("work_dir")+filename+".%Y%m%d%H%M",
 		rotatelogs.WithMaxAge(time.Duration(86400*age)*time.Second),
 		rotatelogs.WithRotationTime(time.Duration(3600*rotation)*time.Second),
 		rotatelogs.WithClock(rotatelogs.UTC),
 	)
 
 	pathMap := lfshook.WriterMap{
+		logrus.DebugLevel: writer,
 		logrus.InfoLevel:  writer,
+		logrus.WarnLevel:  writer,
 		logrus.ErrorLevel: writer,
+		logrus.FatalLevel: writer,
+		logrus.PanicLevel: writer,
 	}
 
-	hook = lfshook.NewHook(
-		pathMap,
-		&logrus.JSONFormatter{},
-	)
+	formatterHook, err := options.String(formatter)
+	if err != nil {
+		logHook.Error(err)
+		formatterHook = formatterDefault
+	}
+
+	if err != nil {
+		logHook.Warn("Useing defaul values!")
+	}
+
+	switch formatterHook {
+	case "json":
+		hook = lfshook.NewHook(
+			pathMap,
+			&logrus.JSONFormatter{},
+		)
+
+	default:
+		hook = lfshook.NewHook(
+			pathMap,
+			&logrus.TextFormatter{},
+		)
+	}
 
 	if err = options.ToObject(&hook); err != nil {
 		return
