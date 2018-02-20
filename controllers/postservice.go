84c5afdc8f732543489a18af5883a17dcfb4830b
Denis Kononenko <denis1kononenko@gmail.com>
2018-02-20 20:11:24 +0200

    Minor fixe

--- controllers/postservice.go
+++ controllers/postservice.go
@@ -35,7 +35,7 @@ type Post struct{
 //To CREATE new Post in DynamoDB
 func CreateNewPost(svc dynamodbiface.DynamoDBAPI, post Post) (Post, error)  {
 
-  //Request to DynamoDB to CREATE new post with KEY_ATTRIBUTE "post_id" (TimeStamp)
+  //Request to DynamoDB to CREATE new post with KEY_ATTRIBUTE "post_id"
   input := &dynamodb.PutItemInput{
     Item: map[string]*dynamodb.AttributeValue{
       "post_id": {
@@ -67,42 +67,49 @@ func CreateNewPost(svc dynamodbiface.DynamoDBAPI, post Post) (Post, error)  {
   //Get result
   result, err := svc.PutItem(input)
 
-  //ERROR block (from AWS SDK GO DynamoDB documentation)
+  //ERROR block
   if err != nil {
     if aerr, ok := err.(awserr.Error); ok {
       switch aerr.Code() {
       case dynamodb.ErrCodeConditionalCheckFailedException:
-        fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+        log.Error(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
       case dynamodb.ErrCodeProvisionedThroughputExceededException:
-        fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
       case dynamodb.ErrCodeResourceNotFoundException:
-        fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+        log.Error(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
       case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
-        fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
       case dynamodb.ErrCodeInternalServerError:
-        fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+        log.Error(dynamodb.ErrCodeInternalServerError, aerr.Error())
       default:
-        fmt.Println(aerr.Error())
+        log.Error(aerr.Error())
       }
     } else {
       // Print the error, cast err to awserr.Error to get the Code and
       // Message from an error.
-      fmt.Println(err.Error())
+      log.Error(err.Error())
     }
     return post, errors.New("Error")
   }
   _ = result
+
+  //logging
   log.Info("Post created successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "Post created": 1,
   }
+
   return post, nil
 }
 
 //To GET Post by POST_ID from DynamoDB
 func GetPost(svc dynamodbiface.DynamoDBAPI, post_id string) (Post, error){
   var post Post
+
   post.PostID = post_id
+
   //Request to DynamoDB to GET post by "post_id"
   input := &dynamodb.GetItemInput{
     Key: map[string]*dynamodb.AttributeValue{
@@ -116,23 +123,23 @@ func GetPost(svc dynamodbiface.DynamoDBAPI, post_id string) (Post, error){
   //Get result
   result, err := svc.GetItem(input)
 
-  //ERROR block (from AWS SDK GO DynamoDB documentation)
+  //ERROR block
   if err != nil {
     if aerr, ok := err.(awserr.Error); ok {
       switch aerr.Code() {
       case dynamodb.ErrCodeProvisionedThroughputExceededException:
-        fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
       case dynamodb.ErrCodeResourceNotFoundException:
-        fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+        log.Error(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
       case dynamodb.ErrCodeInternalServerError:
-        fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+        log.Error(dynamodb.ErrCodeInternalServerError, aerr.Error())
       default:
-        fmt.Println(aerr.Error())
+        log.Error(aerr.Error())
       }
     } else {
       // Print the error, cast err to awserr.Error to get the Code and
       // Message from an error.
-      fmt.Println(err.Error())
+      log.Error(err.Error())
     }
   }
 
@@ -145,11 +152,19 @@ func GetPost(svc dynamodbiface.DynamoDBAPI, post_id string) (Post, error){
   //Unmarshal result to Post structure
   err = dynamodbattribute.UnmarshalMap(result.Item, &post)
 
-  _ = result
+  if err != nil {
+    log.Error("Error to unmarshal")
+    return post, errors.New("Error to unmarshal")
+  }
+
+  //logging
   log.Info("Get post by post id successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int{
     "Get post by post id": 1,
   }
+
   return post, nil
 }
 
@@ -183,32 +198,37 @@ func GetPostByUserID(svc dynamodbiface.DynamoDBAPI, user_id string) ([]Post, err
 
   //Check if POST exists in table, if not: RESPONSE 204 - "No Content"
   if len(result.Items) == 0 {
-    return outputPost, err
+    return outputPost, errors.New("No content")
   }
 
   //Loop for encoding multiples post which was made by "user_id"
   for _, i := range result.Items {
     post := Post{}
+
     //Unmarshal result to Post structure
     err = dynamodbattribute.UnmarshalMap(i, &post)
-    outputPost = append(outputPost, post)
 
     //Check error for unmarshalling
     if err != nil {
-      fmt.Println("Got error unmarshalling:")
-      fmt.Println(err.Error())
-      os.Exit(1)
+      log.Error("Got error unmarshalling:")
+      return outputPost, errors.New("Error unmarshalling")
     }
+
+    outputPost = append(outputPost, post)
   }
-  _ = result
+
+  //logging
   log.Info("Get post by user id successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "Get post by user id": 1,
   }
+
   return outputPost, nil
 }
 
-//To UPDATE Post TITLE and TEXT in DynamoDB
+//To UPDATE Post TITLE, TEXT, POST_LAST_UPDATE in DynamoDB
 func UpdatePost(svc dynamodbiface.DynamoDBAPI, post Post) (Post, error){
 
   //Request to DynamoDB to UPDATE Item in table
@@ -248,39 +268,46 @@ func UpdatePost(svc dynamodbiface.DynamoDBAPI, post Post) (Post, error){
     if aerr, ok := err.(awserr.Error); ok {
       switch aerr.Code() {
       case dynamodb.ErrCodeConditionalCheckFailedException:
-        fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+        log.Error(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
       case dynamodb.ErrCodeProvisionedThroughputExceededException:
-        fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
       case dynamodb.ErrCodeResourceNotFoundException:
-        fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+        log.Error(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
       case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
-        fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
       case dynamodb.ErrCodeInternalServerError:
-        fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+        log.Error(dynamodb.ErrCodeInternalServerError, aerr.Error())
       default:
-        fmt.Println(aerr.Error())
+        log.Error(aerr.Error())
       }
     } else {
       // Print the error, cast err to awserr.Error to get the Code and
       // Message from an error.
-      fmt.Println(err.Error())
+      log.Error(err.Error())
     }
-    return post, err
+    return post, errors.New("Error")
   }
 
   _ = result
+
+  //logging
   log.Info("Update post successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "Update post": 1,
   }
+
   return post, nil
 }
 
 //To DELETE Post in DynamoDB
 func DeletePost(svcd dynamodbiface.DynamoDBAPI, svcs s3iface.S3API, post_id string) string{
-
+  //Get Post from DynamoDB
   post, err := GetPost(svcd, post_id)
 
+  //Check if Post has file on S3
+  //if true -> Delete file from S3
   if post.FileLink != "NULL" {
     DeleteFileFromS3(post.FileLink, svcs)
   }
@@ -303,28 +330,31 @@ func DeletePost(svcd dynamodbiface.DynamoDBAPI, svcs s3iface.S3API, post_id stri
     if aerr, ok := err.(awserr.Error); ok {
       switch aerr.Code() {
       case dynamodb.ErrCodeConditionalCheckFailedException:
-        fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+        log.Error(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
       case dynamodb.ErrCodeProvisionedThroughputExceededException:
-        fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
       case dynamodb.ErrCodeResourceNotFoundException:
-        fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+        log.Error(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
       case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
-        fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+        log.Error(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
       case dynamodb.ErrCodeInternalServerError:
-        fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+        log.Error(dynamodb.ErrCodeInternalServerError, aerr.Error())
       default:
-        fmt.Println(aerr.Error())
+        log.Error(aerr.Error())
       }
     } else {
       // Print the error, cast err to awserr.Error to get the Code and
       // Message from an error.
-      fmt.Println(err.Error())
+      log.Error(err.Error())
     }
-    return "Error"
   }
 
   _ = result
+
+  //logging
   log.Info("Delete post successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "Delete post": 1,
   }
@@ -336,6 +366,8 @@ func DeletePost(svcd dynamodbiface.DynamoDBAPI, svcs s3iface.S3API, post_id stri
 func UploadFileToS3(svc s3iface.S3API, f multipart.File, handl * multipart.FileHeader) string{
   // Create an uploader with the session and default options
   uploader := s3manager.NewUploaderWithClient(svc)
+
+  //Get type of file
   fileType := handl.Filename[strings.LastIndexAny(handl.Filename, "."):]
   fileType = strings.ToLower(fileType)
 
@@ -344,28 +376,38 @@ func UploadFileToS3(svc s3iface.S3API, f multipart.File, handl * multipart.FileH
   if err != nil{
     fmt.Printf("error: %v\n", err)
   }
+
   // Upload the file to S3.
   result, err := uploader.Upload(&s3manager.UploadInput{
     Bucket: aws.String(conf.AwsBucketName),
     Key:    aws.String(uuid + fileType),
     Body:   f,
   })
+
   if err != nil{
     fmt.Errorf("failed to upload file, %v", err)
     return "failed to upload file"
   }
 
   _ = result
+
+  //logging
   log.Info("File uploaded successfully")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "File upload": 1,
   }
+
+  //Get link to store it in DynamoDB
   link := "/uploads/" + uuid + fileType
+
   return link
 }
 
 //To DELETE file from S3 when DELETE Post
 func DeleteFileFromS3(file_link string, svc s3iface.S3API) {
+  //Get file name from DynamoDB, trimming "/uploads/"
   filename := strings.TrimPrefix(file_link, "/uploads/")
 
   input := &s3.DeleteObjectsInput{
@@ -381,21 +423,26 @@ func DeleteFileFromS3(file_link string, svc s3iface.S3API) {
   }
 
   result, err := svc.DeleteObjects(input)
+
   if err != nil {
     if aerr, ok := err.(awserr.Error); ok {
       switch aerr.Code() {
       default:
-        fmt.Println(aerr.Error())
+        log.Error(aerr.Error())
       }
     } else {
       // Print the error, cast err to awserr.Error to get the Code and
       // Message from an error.
-      fmt.Println(err.Error())
+      log.Error(err.Error())
     }
     return
   }
   _ = result
+
+  //logging
   log.Info("File deleted")
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "File deleted": 1,
   }
@@ -414,14 +461,20 @@ func DownloadFileFromS3(svc s3iface.S3API, fileName string) (*aws.WriteAtBuffer,
     Bucket: aws.String(conf.AwsBucketName),
     Key:    aws.String(fileName),
   })
+
   if err != nil {
     fmt.Errorf("failed to download file, %v", err)
     return buff, errors.New("Error")
   }
+
+  //logging
   log.Info("File downloaded, %d bytes\n", n)
+
+  //statistic
   humstat.SendStat <- map[string]int {
     "File downloaded": 1,
   }
+
   return buff, nil
 }
 
@@ -439,7 +492,3 @@ func NewUUID() (string, error) {
   log.Info("New UUID has generated")
   return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
-
-
-
-
