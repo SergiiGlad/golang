52f4cf59d6079ebdbbd81cfaf81e7680b1b39d33
sergiiglad <sergii.gladchenko@gmail.com>
2018-02-21 00:02:37 +0200

    Add files via upload

--- concurency.go
+++ concurency.go
@@ -15,21 +15,34 @@ func summary(arr []int, c chan int) {
 
 }
 
+func buffer(buf int, c chan int) {
+
+	for i := 0; i < buf; i++ {
+		fmt.Printf(" %d send to channel\n", i)
+		c <- i
+	}
+}
+
 func main() {
 
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
 
-	c := make(chan int)
+	c := make(chan int, 2)
 
-	go summary(arr, c)              //45
+	go summary(arr, c)              //45 long running
+	go summary(arr[len(arr)/2:], c) //30
 	go summary(arr[:1], c)          //1
 	go summary(arr[:2], c)          //3
-	go summary(arr[len(arr)/2:], c) //30
 
 	x := <-c // 45
-	y := <-c // 1
-	x = <-c  //3
+	y := <-c // 30
+	x = <-c  //1
 
-	fmt.Printf("x :%x  y : %d\n", x, y) // 3 1 or 1 45
+	fmt.Printf("x :%d  y : %d\n", x, y) //
+
+	//Sends to a buffered channel block only when the buffer is full. Receives block when the buffer is empty.
+	go buffer(5, c)
+	fmt.Printf("read from chan %d\n", <-c)
+	fmt.Printf("read from chan %d\n", <-c)
 
 }
